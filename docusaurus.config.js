af064b8282e439e4a3997388c163c5318295c3ee
Arash Hatami <hatamiarash7@gmail.com>
2020-04-09 01:27:56 +0430

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,96 +1,100 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'My Site',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
-      links: [
-        {
-          to: 'docs/doc1',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
+    title: "آرش حاتمی",
+    tagline: "DevOps - SRE / Network Eng",
+    url: "https://arash-hatami.ir",
+    baseUrl: "/",
+    favicon: "img/favicon.ico",
+    organizationName: "hatamiarash7",
+    projectName: "MyWebSite",
+    themeConfig: {
+        navbar: {
+            title: "آرش حاتمی",
+            logo: {
+                alt: "Logo",
+                src: "img/logo.svg",
+            },
+            links: [
+                {
+                    to: "docs/doc1",
+                    activeBasePath: "docs",
+                    label: "پروژه ها",
+                    position: "left",
+                },
+                {
+                    to: "blog",
+                    label: "نوشته ها",
+                    position: "left",
+                },
+            ],
         },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+        footer: {
+            style: "dark",
+            links: [
+                {
+                    title: "دیگر سایت ها",
+                    items: [
+                        {
+                            label: "Code Guide",
+                            href: "https://cg.arash-hatami.ir",
+                        },
+                        {
+                            label: "DNS Lookup",
+                            href: "https://dns.arash-hatami.ir",
+                        },
+                    ],
+                },
+                {
+                    title: "پروفایل ها",
+                    items: [
+                        {
+                            label: "GitHub",
+                            href: "https://github.com/hatamiarash7",
+                        },
+                        {
+                            label: "Stack Overflow",
+                            href:
+                                "https://stackoverflow.com/users/4905220/arash-hatami",
+                        },
+                        {
+                            label: "Docker Hub",
+                            href: "https://hub.docker.com/u/hatamiarash7",
+                        },
+                    ],
+                },
+                {
+                    title: "شبکه های اجتماعی",
+                    items: [
+                        {
+                            label: "LinkedIn",
+                            href: "https://www.linkedin.com/in/hatamiarash7",
+                        },
+                        {
+                            label: "Instagram",
+                            href: "https://instagram.com/hatamiarash7",
+                        },
+                        {
+                            label: "Twitter",
+                            href: "https://twitter.com/hatamiarash7",
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Arash Hatami`,
         },
-      ],
     },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
+    presets: [
+        [
+            "@docusaurus/preset-classic",
             {
-              label: 'Style Guide',
-              to: 'docs/doc1',
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                    editUrl:
+                        "https://github.com/facebook/docusaurus/edit/master/website/",
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
             },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'Social',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-    },
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+        ],
     ],
-  ],
 };
