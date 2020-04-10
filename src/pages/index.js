ff3b5dbe6d9b81bb541228d84955875a9fb1bc1f
Arash Hatami <hatamiarash7@gmail.com>
2020-04-10 14:48:24 +0430

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,34 +10,17 @@ const features = [
     {
         title: <>Programming</>,
         imageUrl: "img/intro-1.svg",
-        description: (
-            <>
-                Docusaurus was designed from the ground up to be easily
-                installed and used to get your website up and running quickly.
-            </>
-        ),
+        description: <>a</>,
     },
     {
         title: <>Focus on What Matters</>,
         imageUrl: "img/intro-2.svg",
-        description: (
-            <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the
-                chores. Go ahead and move your docs into the <code>docs</code>{" "}
-                directory.
-            </>
-        ),
+        description: <>b</>,
     },
     {
         title: <>Powered by React</>,
         imageUrl: "img/intro-3.svg",
-        description: (
-            <>
-                Extend or customize your website layout by reusing React.
-                Docusaurus can be extended while reusing the same header and
-                footer.
-            </>
-        ),
+        description: <>c</>,
     },
 ];
 
