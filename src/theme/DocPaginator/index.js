1c26c76e5d8605e48dafb5dbb76a5a237183714e
Arash Hatami <hatamiarash7@gmail.com>
2020-04-28 22:39:07 +0430

    update style

--- src/theme/DocPaginator/index.js
+++ src/theme/DocPaginator/index.js
@@ -5,38 +5,46 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-import Link from '@docusaurus/Link';
+import React from "react";
+import Link from "@docusaurus/Link";
 
 function DocPaginator(props) {
-  const {metadata} = props;
+    const { metadata } = props;
 
-  return (
-    <nav className="pagination-nav">
-      <div className="pagination-nav__item">
-        {metadata.previous && (
-          <Link
-            className="pagination-nav__link"
-            to={metadata.previous.permalink}>
-            <div className="pagination-nav__link--sublabel">Previous</div>
-            <div className="pagination-nav__link--label">
-              &laquo; {metadata.previous.title}
+    return (
+        <nav className="pagination-nav">
+            <div className="pagination-nav__item">
+                {metadata.previous && (
+                    <Link
+                        className="pagination-nav__link"
+                        to={metadata.previous.permalink}
+                    >
+                        <div className="pagination-nav__link--sublabel font">
+                            پروژه قبلی
+                        </div>
+                        <div className="pagination-nav__link--label">
+                            {metadata.previous.title}
+                        </div>
+                    </Link>
+                )}
             </div>
-          </Link>
-        )}
-      </div>
-      <div className="pagination-nav__item pagination-nav__item--next">
-        {metadata.next && (
-          <Link className="pagination-nav__link" to={metadata.next.permalink}>
-            <div className="pagination-nav__link--sublabel">Next</div>
-            <div className="pagination-nav__link--label">
-              {metadata.next.title} &raquo;
+            <div className="pagination-nav__item pagination-nav__item--next">
+                {metadata.next && (
+                    <Link
+                        className="pagination-nav__link"
+                        to={metadata.next.permalink}
+                    >
+                        <div className="pagination-nav__link--sublabel font">
+                            پروژه بعدی
+                        </div>
+                        <div className="pagination-nav__link--label">
+                            {metadata.next.title}
+                        </div>
+                    </Link>
+                )}
             </div>
-          </Link>
-        )}
-      </div>
-    </nav>
-  );
+        </nav>
+    );
 }
 
 export default DocPaginator;
