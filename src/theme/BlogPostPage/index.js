c7bc6c6337b2eb92cb96c6cfa30a4bf09588a85e
Arash Hatami <hatamiarash7@gmail.com>
2020-05-22 14:04:33 +0430

    add isso comment

--- src/theme/BlogPostPage/index.js
+++ src/theme/BlogPostPage/index.js
@@ -5,41 +5,58 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React from 'react';
-
-import Layout from '@theme/Layout';
-import BlogPostItem from '@theme/BlogPostItem';
-import BlogPostPaginator from '@theme/BlogPostPaginator';
+import React from "react";
+import { Helmet } from "react-helmet";
+import Layout from "@theme/Layout";
+import BlogPostItem from "@theme/BlogPostItem";
+import BlogPostPaginator from "@theme/BlogPostPaginator";
+import _ from "./isso.scss";
 
 function BlogPostPage(props) {
-  const {content: BlogPostContents} = props;
-  const {frontMatter, metadata} = BlogPostContents;
-  return (
-    <Layout title={metadata.title} description={metadata.description}>
-      {BlogPostContents && (
-        <div className="container margin-vert--xl">
-          <div className="row">
-            <div className="col col--8 col--offset-2">
-              <BlogPostItem
-                frontMatter={frontMatter}
-                metadata={metadata}
-                isBlogPostPage>
-                <BlogPostContents />
-              </BlogPostItem>
-              {(metadata.nextItem || metadata.prevItem) && (
-                <div className="margin-vert--xl">
-                  <BlogPostPaginator
-                    nextItem={metadata.nextItem}
-                    prevItem={metadata.prevItem}
-                  />
+    const { content: BlogPostContents } = props;
+    const { frontMatter, metadata } = BlogPostContents;
+    return (
+        <Layout title={metadata.title} description={metadata.description}>
+            {BlogPostContents && (
+                <div className="container margin-vert--xl">
+                    <div className="row">
+                        <div className="col col--8 col--offset-2">
+                            <BlogPostItem
+                                frontMatter={frontMatter}
+                                metadata={metadata}
+                                isBlogPostPage
+                            >
+                                <BlogPostContents />
+                            </BlogPostItem>
+                            {(metadata.nextItem || metadata.prevItem) && (
+                                <div className="margin-vert--xl">
+                                    <BlogPostPaginator
+                                        nextItem={metadata.nextItem}
+                                        prevItem={metadata.prevItem}
+                                    />
+                                </div>
+                            )}
+                            <Helmet>
+                                <script
+                                    data-isso="http://domain.test:8085/"
+                                    data-isso-lang="fa"
+                                    data-isso-require-author="true"
+                                    data-isso-require-email="true"
+                                    data-isso-reply-to-self="false"
+                                    data-isso-avatar="false"
+                                    data-isso-gravatar="true"
+                                    data-isso-vote="false"
+                                    data-isso-feed="false"
+                                    src="http://domain.test:8085/js/embed.min.js"
+                                ></script>
+                            </Helmet>
+                            <section id="isso-thread"></section>
+                        </div>
+                    </div>
                 </div>
-              )}
-            </div>
-          </div>
-        </div>
-      )}
-    </Layout>
-  );
+            )}
+        </Layout>
+    );
 }
 
 export default BlogPostPage;
