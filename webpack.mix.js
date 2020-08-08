1281d67763c3c0d116a7bad7d74bec9695a428d0
Arash Hatami <hatamiarash7@gmail.com>
2020-08-08 23:34:58 +0430

    configure webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,21 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .sourceMaps();
+
+mix.options({
+    postCss: [
+        require("autoprefixer")({
+            grid: "autoplace"
+        })
+    ]
+});
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
+mix.browserSync("http://localhost:8000");
