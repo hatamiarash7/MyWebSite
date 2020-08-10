b02526b1f20296a6fb8b35946598b832d960cb81
Arash Hatami <hatamiarash7@gmail.com>
2020-08-10 12:14:07 +0430

    Add dashboard assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,31 @@ const mix = require("laravel-mix");
 
 mix.disableNotifications();
 
-mix.js("resources/js/app.js", "public/js")
-    .sass("resources/sass/app.scss", "public/css")
+mix
+    .sass(
+        'resources/sass/vazir.scss',
+        'public/dashboard-assets/css/vazir.css'
+    )
+    .sass(
+        'resources/sass/dashboard/argon.scss',
+        'public/dashboard-assets/css/main.css'
+    )
+    .copyDirectory(
+        'resources/fonts/nucleo',
+        'public/dashboard-assets/fonts/nucleo'
+    )
+    .copyDirectory(
+        'resources/vendor',
+        'public/dashboard-assets/vendor'
+    )
+    .copyDirectory(
+        'resources/img/dashboard',
+        'public/dashboard-assets/img'
+    )
+    .copy(
+        'resources/js/dashboard/argon.min.js',
+        'public/dashboard-assets/js/argon.min.js'
+    )
     .sourceMaps();
 
 mix.options({
