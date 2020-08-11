6d6d8200dfe7b267b9db58966d8c14472922a1e5
Arash Hatami <hatamiarash7@gmail.com>
2020-08-11 15:27:03 +0430

    Update auth contents

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -3,13 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Arash Hatami</div>
-                    <div class="card-body">
-                        Hey there
-                    </div>
-                </div>
+            <div class="card-body text-center">
+                :)
             </div>
         </div>
     </div>
