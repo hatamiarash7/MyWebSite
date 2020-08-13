42b1c0433de5ecf3fd127486c4b8a4105b78a97a
Arash Hatami <hatamiarash7@gmail.com>
2020-08-13 19:06:08 +0430

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth'], 'prefix' => 'dashboard', 'as' => 'dashboard::'], function () {
+    Route::get('/', ['as' => 'index', 'uses' => 'Dashboard\DashboardController@index']);
+
+    Route::group(['prefix' => 'category', 'as' => 'category.'], function () {
+        Route::get('/index', ['as' => 'index', 'uses' => 'Dashboard\CategoryController@index']);
+        Route::get('/create', ['as' => 'create', 'uses' => 'Dashboard\CategoryController@create']);
+        Route::post('/save', ['as' => 'save', 'uses' => 'Dashboard\CategoryController@save']);
+        Route::get('/edit/{id}', ['as' => 'edit', 'uses' => 'Dashboard\CategoryController@edit']);
+        Route::post('/update/{id}', ['as' => 'update', 'uses' => 'Dashboard\CategoryController@update']);
+        Route::get('/delete/{id}', ['as' => 'delete', 'uses' => 'Dashboard\CategoryController@delete']);
+    });
+
+    Route::group(['prefix' => 'post', 'as' => 'post.'], function () {
+        Route::get('/index', ['as' => 'index', 'uses' => 'Dashboard\PostController@index']);
+        Route::get('/create', ['as' => 'create', 'uses' => 'Dashboard\PostController@create']);
+        Route::post('/save', ['as' => 'save', 'uses' => 'Dashboard\PostController@save']);
+        Route::get('/edit/{id}', ['as' => 'edit', 'uses' => 'Dashboard\PostController@edit']);
+        Route::post('/update/{id}', ['as' => 'update', 'uses' => 'Dashboard\PostController@update']);
+        Route::get('/delete/{id}', ['as' => 'delete', 'uses' => 'Dashboard\PostController@delete']);
+    });
+});
